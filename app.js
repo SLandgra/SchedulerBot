cc77d547796ea6c12f007fd71b2c2434e5395bc7
hqiao97 <hqiao@wellesley.edu>
2017-08-01 13:35:38 -0700

    completed slack and API.AI connection

--- app.js
+++ app.js
@@ -3,42 +3,149 @@ var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var path = require('path');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 var mongoose = require('mongoose');
 var connect = process.env.MONGODB_URI;
+var axios = require('axios');
+var rtm = require('./bot.js');
+rtm.start();
 
-var REQUIRED_ENV = "SECRET MONGODB_URI".split(" ");
+// var REQUIRED_ENV = "SECRET MONGODB_URI".split(" ");
+//
+// REQUIRED_ENV.forEach(function(el) {
+//   if (!process.env[el]){
+//     console.error("Missing required env var " + el);
+//     process.exit(1);
+//   }
+// });
+//
+//
+// mongoose.connect(connect);
+//
+// var models = require('./models');
+//
+var routes = require('./routes/routes');
+var auth = require('./routes/auth');
+var app = express();
+//
+// // view engine setup
+// var hbs = require('express-handlebars')({
+//   defaultLayout: 'main',
+//   extname: '.hbs'
+// });
+// app.engine('hbs', hbs);
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+//
+app.use(logger('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+//
+// // Passport
+// app.use(session({
+//   secret: process.env.SECRET,
+//   store: new MongoStore({ mongooseConnection: mongoose.connection })
+// }));
+//
+//
+// app.use(passport.initialize());
+// app.use(passport.session());
+//
+// passport.serializeUser(function(user, done) {
+//   done(null, user._id);
+// });
+//
+// passport.deserializeUser(function(id, done) {
+//   models.User.findById(id, done);
+// });
+//
+// // passport strategy
+// passport.use(new LocalStrategy(function(username, password, done) {
+//   // Find the user with the given username
+//   models.User.findOne({ username: username }, function (err, user) {
+//     // if there's an error, finish trying to authenticate (auth failed)
+//     if (err) {
+//       console.error('Error fetching user in LocalStrategy', err);
+//       return done(err);
+//     }
+//     // if no user present, auth failed
+//     if (!user) {
+//       return done(null, false, { message: 'Incorrect username.' });
+//     }
+//     // if passwords do not match, auth failed
+//     if (user.password !== password) {
+//       return done(null, false, { message: 'Incorrect password.' });
+//     }
+//     // auth has has succeeded
+//     return done(null, user);
+//   });
+// }
+// ));
+//
+// app.use('/', auth(passport));
+// app.use('/', routes);
+//
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // error handlers
+//
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
 
-REQUIRED_ENV.forEach(function(el) {
-  if (!process.env[el]){
-    console.error("Missing required env var " + el);
-    process.exit(1);
+// production error handler
+// no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
+// app.use(bodyParser.urlencoded({extended: true}));
+//
+
+app.get('/', function(req, res){
+  res.send('Hello!');
+});
+
+app.post('/interactive', function(req, res){
+  console.log('REQ.BODY*****************');
+  console.log('ORIGINAL MESSAGE*****************');
+  console.log(JSON.parse(req.body.payload).original_message);
+  var doConfirm = JSON.parse(req.body.payload).actions[0].name === 'confirm';
+  if (doConfirm) {
+    console.log(doConfirm);
+
+  } else {
+    console.log(doConfirm);
   }
 });
 
 
-mongoose.connect(connect);
 
-var models = require('./models');
 
-var routes = require('./routes/routes');
-var app = express();
 
-// view engine setup
-var hbs = require('express-handlebars')({
-  defaultLayout: 'main',
-  extname: '.hbs'
-});
-app.engine('hbs', hbs);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 
-app.use(logger('tiny'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 
 
 
---
+++ bot.js
@@ -0,0 +1,95 @@
+var axios = require('axios');
+
+var RtmClient = require('@slack/client').RtmClient;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+var token = process.env.SLACK_BOT_TOKEN || '';
+var apiToken = process.env.API_ACCESS_TOKEN;
+
+var rtm = new RtmClient(token);
+
+// WEB API
+var WebClient = require('@slack/client').WebClient;
+var web = new WebClient(token);
+
+let channel;
+
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  var authenticated = true;
+  if (!authenticated) {
+        // make user model
+  } else {
+    axios({
+      method: 'post',
+      url: 'https://api.api.ai/v1/query?v=20150910',
+      headers: {
+        'Authorization': 'Bearer ' + apiToken,
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      data: {
+        query: message.text,
+        lang: "en",
+        sessionId: message.user
+      }
+    }).then((resp) => {
+      console.log('AI RESPONSE DATA RESULT ****************************************');
+      console.log(resp.data.result);
+      var aiResponse = resp.data.result.fulfillment.speech;
+      if (resp.data.result.action === 'input.reminder.add' && resp.data.result.actionIncomplete === false){
+        var interactive = {
+          "attachments": [
+            {
+              "text": `${aiResponse} <@${message.user}>!`,
+              "fallback": "You are unable to respond to the request",
+              "callback_id": "confirmation",
+              "color": "#3AA3E3",
+              "attachment_type": "default",
+              "actions": [
+                {
+                    "name": "confirm",
+                    "text": "Confirm",
+                    "type": "button",
+                    "value": "confirm"
+                },
+                {
+                    "name": "cancel",
+                    "text": "Cancel",
+                    "type": "button",
+                    "value": "cancel"
+                },
+              ]
+            }
+          ]
+        }
+
+        web.chat.postMessage(message.channel, 'Do you want to confirm your reminder?', interactive, function(err, res) {
+          if (err) {
+            console.log('POSTING Error:', err);
+          } else {
+            console.log('interactive sent');
+          }
+        });
+
+      } else {
+        rtm.sendMessage(`${aiResponse} <@${message.user}>!`, message.channel);
+      }
+    }).catch(error => {
+      console.log('axios error');
+    });
+  }
+});
+
+// Wait for the client to connect
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
+  // Get the user's name
+  var user = rtm.dataStore.getUserById(rtm.activeUserId);
+
+  // Get the team's name
+  var team = rtm.dataStore.getTeamById(rtm.activeTeamId);
+
+  // Log the slack team name and the bot's name
+  console.log('Connected to ' + team.name + ' as ' + user.name);
+});
+
+module.exports = rtm;
