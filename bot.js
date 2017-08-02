03f3cac670772cb9366270138b00e9e45ef545ae
hqiao97 <hqiao@wellesley.edu>
2017-08-02 14:37:28 -0700

    can schedule remind on calender

--- bot.js
+++ bot.js
@@ -32,82 +32,83 @@ rtm.on(RTM_EVENTS.MESSAGE, async function(message) {
 
   var authenticated = user.google;
   if (!authenticated && message.subtype !== 'bot_message') {
-    var url = 'http://localhost:3000/connect?auth_id='+user._id;
+    var url = 'https://cb784a5d.ngrok.io/connect?auth_id='+user._id;
     console.log("message*************", message);
     rtm.sendMessage(`<@${message.user}>! Please click on the link to authenticate! ${url}`, message.channel);
     return;
-  }
-  axios({
-    method: 'post',
-    url: 'https://api.api.ai/v1/query?v=20150910',
-    headers: {
-      'Authorization': 'Bearer ' + apiToken,
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    data: {
-      query: message.text,
-      lang: "en",
-      sessionId: message.user
-    }
-  }).then((resp) => {
-    console.log('AI RESPONSE DATA RESULT ****************************************');
-    console.log(resp.data.result);
-    var aiResponse = resp.data.result.fulfillment.speech;
-    if (resp.data.result.action === 'input.reminder.add' && resp.data.result.actionIncomplete === false){
+  } else {
+    axios({
+      method: 'post',
+      url: 'https://api.api.ai/v1/query?v=20150910',
+      headers: {
+        'Authorization': 'Bearer ' + apiToken,
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      data: {
+        query: message.text,
+        lang: "en",
+        sessionId: message.user
+      }
+    }).then((resp) => {
+      console.log('AI RESPONSE DATA RESULT ****************************************');
+      console.log(resp.data.result);
+      var aiResponse = resp.data.result.fulfillment.speech;
+      if (resp.data.result.action === 'input.reminder.add' && resp.data.result.actionIncomplete === false){
 
-      new Task({
-        subject: resp.data.result.parameters.subject[0],
-        day: resp.data.result.parameters.date[0],
-        user_id: message.user,
-        pending: true
-      }).save(function(err) {
-        if (err) {
-          console.log('saving err!');
-        } else {
-          var interactive = {
-            "attachments": [
-              {
-                "text": `${aiResponse} <@${message.user}>!`,
-                "fallback": "You are unable to respond to the request",
-                "callback_id": "confirmation",
-                "color": "#3AA3E3",
-                "attachment_type": "default",
-                "actions": [
-                  {
-                      "name": "confirm",
-                      "text": "Confirm",
-                      "type": "button",
-                      "value": "confirm"
-                  },
-                  {
-                      "name": "cancel",
-                      "text": "Cancel",
-                      "type": "button",
-                      "value": "cancel"
-                  },
-                ]
-              }
-            ]
-          }
-
-          console.log("I'm before PostMessage");
-          web.chat.postMessage(message.channel, 'Do you want to confirm your reminder?', interactive, function(err, res) {
-            if (err) {
-              console.log('POSTING Error:', err);
-            } else {
-              console.log('interactive sent');
+        new Task({
+          subject: resp.data.result.parameters.subject[0],
+          day: resp.data.result.parameters.date[0],
+          user_id: message.user,
+          pending: true
+        }).save(function(err) {
+          if (err) {
+            console.log('saving err!');
+          } else {
+            var interactive = {
+              "attachments": [
+                {
+                  "text": `${aiResponse} <@${message.user}>!`,
+                  "fallback": "You are unable to respond to the request",
+                  "callback_id": "confirmation",
+                  "color": "#3AA3E3",
+                  "attachment_type": "default",
+                  "actions": [
+                    {
+                        "name": "confirm",
+                        "text": "Confirm",
+                        "type": "button",
+                        "value": "confirm"
+                    },
+                    {
+                        "name": "cancel",
+                        "text": "Cancel",
+                        "type": "button",
+                        "value": "cancel"
+                    },
+                  ]
+                }
+              ]
             }
-          });
 
-        }
-      })
+            console.log("I'm before PostMessage");
+            web.chat.postMessage(message.channel, 'Do you want to confirm your reminder?', interactive, function(err, res) {
+              if (err) {
+                console.log('POSTING Error:', err);
+              } else {
+                console.log('interactive sent');
+              }
+            });
 
-    } else {
-      rtm.sendMessage(`${aiResponse} <@${message.user}>!`, message.channel);
-    }
-  }).catch(error => {
-    console.log('axios error');
-  });
+          }
+        })
+
+      } else {
+        rtm.sendMessage(`${aiResponse} <@${message.user}>!`, message.channel);
+      }
+    }).catch(error => {
+      console.log('axios error');
+    }); //End of axios request
+  }
 });
 
 // Wait for the client to connect
